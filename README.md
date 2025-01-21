5c6cca96925e5508c6ee5fd1fe38eb852ad9da70
sultanbee786 <sulthanbeeshaik31@gmail.com>
2025-01-21 20:28:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# JS If Statement Lab
+
+
+## Description
+
+In this lab you'll learn how to use JavaScript to create conditional logic then make your HTML page reflect the outcome; this is logic that will only trigger will a certian condition is met.
+
+
+## Where to start
+
+Begin by opening and viewing the "IfStatement.html" file; the file itself contains this lab's instructions.
+
+Files to edit:
+- IfStatement.html
+
+
+
+
+
---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,86 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumTest {
+
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        // Set up ChromeDriver path
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
+
+        // Get file
+        File file = new File("src/main/IfStatement.html");
+        String path = "file://" + file.getAbsolutePath();
+
+        // Create a new ChromeDriver instance
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        webDriver = new ChromeDriver(options);
+
+
+        // Open the HTML file
+        webDriver.get(path);
+    }
+
+    @Test
+    public void testIncrementButton() {
+
+        // Find the increment button and click it
+        WebElement incrementButton = webDriver.findElement(By.id("counter"));
+        incrementButton.click();
+
+        // Find the display element and verify the incremented value
+        WebElement displayElement = webDriver.findElement(By.id("display"));
+        assertEquals("2", displayElement.getText());
+        incrementButton.click();
+
+        // Find the display element and verify the incremented value
+        assertEquals("3", displayElement.getText());
+    }
+
+    @Test
+    public void testCheckButton() {
+
+        // Find the checkButton and click it
+        WebElement countButton = webDriver.findElement(By.id("counter"));
+        countButton.click();
+
+        // Find the lightText element and verify the text based on the initial value of lightOn
+        WebElement evenOrOddTextElement = webDriver.findElement(By.id("evenOrOddText"));
+        String expectedText = "even"; // Assuming number is initially 1, and 2 after a click
+        assertEquals(expectedText, evenOrOddTextElement.getText());
+        
+    }
+    @Test
+    public void testCheckButton2Clicks() {
+
+        // Find the checkButton and click it
+        WebElement countButton = webDriver.findElement(By.id("counter"));
+        countButton.click();
+        countButton.click();
+
+        // Find the lightText element and verify the text based on the initial value of lightOn
+        WebElement evenOrOddTextElement = webDriver.findElement(By.id("evenOrOddText"));
+        String expectedText = "odd"; // Assuming number is initially 1, and 2 after a click
+        assertEquals(expectedText, evenOrOddTextElement.getText());
+        
+    }
+
+    @After
+    public void tearDown() {
+        // Close the browser
+        webDriver.quit();
+    }
+}
